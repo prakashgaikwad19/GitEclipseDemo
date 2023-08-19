bac12a853ff48942c6d8d5c9d83fed6eeef06e79
prakashgaikwad19 <prakash1998@gmail.com>
2023-08-19 10:00:05 +0530

    1st commit:19-08-2023

---
+++ GitEclipseDemo/src/package1/GitTokens.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class GitTokens {
+	//	prakashgaikwad19
+	//	PettyProject1 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
+	
+	//	ESOperations
+	//	https://github.com/prakashgaikwad19/EsOperations.git
+}
